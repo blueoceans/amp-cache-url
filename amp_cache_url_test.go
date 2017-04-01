888fd843d76ea6232f65403fd09409b3c9083921
ENDOH takanao <djmchl@gmail.com>
2017-04-01 16:24:58 +1300

    refactor(golint): Url to URL

--- amp_cache_url_test.go
+++ amp_cache_url_test.go
@@ -18,38 +18,38 @@ var vtests = []struct {
 		"https://example-com.cdn.ampproject.org/r/s/example.com/i.woff"},
 }
 
-func TestIsCacheUrl(t *testing.T) {
+func TestIsCacheURL(t *testing.T) {
 	for _, vt := range vtests {
-		if IsCacheUrl(vt.url) != false {
+		if IsCacheURL(vt.url) != false {
 			t.Errorf("%q, want `false`", vt.url)
 		}
-		if IsCacheUrl(vt.ampCacheURL) != true {
+		if IsCacheURL(vt.ampCacheURL) != true {
 			t.Errorf("%q, want `true`", vt.ampCacheURL)
 		}
 	}
 }
 
-func TestGetCacheUrl(t *testing.T) {
+func TestGetCacheURL(t *testing.T) {
 	for _, vt := range vtests {
-		if url, err := GetCacheUrl(vt.url); err != nil {
+		if url, err := GetCacheURL(vt.url); err != nil {
 			t.Errorf("can not get url: %v", err)
 		} else if url != vt.ampCacheURL {
 			t.Errorf("%q, want %q, got %q", vt.url, vt.ampCacheURL, url)
 		}
-		if _, err := GetCacheUrl(vt.ampCacheURL); err == nil {
+		if _, err := GetCacheURL(vt.ampCacheURL); err == nil {
 			t.Errorf("%q, want `error`", vt.ampCacheURL)
 		}
 	}
 }
 
-func TestGetOriginUrl(t *testing.T) {
+func TestGetOriginURL(t *testing.T) {
 	for _, vt := range vtests {
-		if url, err := GetOriginUrl(vt.ampCacheURL); err != nil {
+		if url, err := GetOriginURL(vt.ampCacheURL); err != nil {
 			t.Errorf("can not get url: %v", err)
 		} else if url != vt.url {
 			t.Errorf("%q, want %q, got %q", vt.ampCacheURL, vt.url, url)
 		}
-		if _, err := GetOriginUrl(vt.url); err == nil {
+		if _, err := GetOriginURL(vt.url); err == nil {
 			t.Errorf("%q, want `error`", vt.url)
 		}
 	}
